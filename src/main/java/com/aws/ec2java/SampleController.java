ab518722b39defe2fc944d77d0f7fde52ff6b22e
jmdozza <jmendozaw@unal.edu.co>
2026-03-23 18:47:19 -0500

    sample controller added

---
+++ src/main/java/com/aws/ec2java/SampleController.java
@@ -0,0 +1,24 @@
+package com.aws.ec2java;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("/api/vi/aws")
+public class SampleController {
+    
+    @GetMapping
+    public ResponseEntity<String> getMethodName() {
+        return ResponseEntity.ok("Hello Aws World, this is a get response");
+    }   
+
+    @PostMapping
+    public ResponseEntity<String> postMethodName(@RequestBody String entity) {
+        return ResponseEntity.ok("Hello, this is a post response");
+    }
+    
+}
---
+++ src/test/java/com/aws/ec2java/Ec2javaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.ec2java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec2javaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
